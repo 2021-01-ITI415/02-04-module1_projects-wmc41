c8182be401c9bd7f0dfd24b6572131c48f42bee6
wmc41 <wmc41@users.noreply.github.com>
2021-02-04 00:21:42 -0500

    Basket Script
    Added script which created 3 basket prefabs at the start of the game.

---
+++ Assets/ApplePicker1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBaksetGo = Instantiate(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBaksetGo.transform.position = pos;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
