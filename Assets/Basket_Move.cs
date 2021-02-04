ed6a66e44b694649ac5550ce657f6b6868bb4dc6
wmc41 <wmc41@users.noreply.github.com>
2021-02-04 03:26:07 -0500

    Score Counter Script
    Created script which adds points to score counter every time an apple is caught .

--- Assets/Basket_Move.cs
+++ Assets/Basket_Move.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket_Move : MonoBehaviour
 {
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
 
     }
 
@@ -28,6 +35,9 @@ public class Basket_Move : MonoBehaviour
         if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
