587c8ca5d70facb4b221864420e777aeba2a1f89
wmc41 <wmc41@users.noreply.github.com>
2021-02-04 02:04:58 -0500

    Basket Moving Script
    Added script which allows baskets to be moved by mouse.

---
+++ Assets/Basket_Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector3 mousePos2d = Input.mousePosition;
+        mousePos2d.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2d);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
