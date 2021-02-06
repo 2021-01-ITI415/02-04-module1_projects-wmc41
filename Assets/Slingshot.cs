b9d0df1ba39441d0e84df443321c4b637e190ba7
wmc41 <wmc41@users.noreply.github.com>
2021-02-06 09:37:30 -0500

    Projectile Physics
    Added Physic Material which made projectile physics more realistic.

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -75,6 +75,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
         
